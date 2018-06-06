ecc66f8f442b7c2343cc9ee4ffaaea1b2f4b7836
Jai <contactjaitiwari@gmail.com>
2018-06-06 12:37:18 -0400

    first deployment

--- server.js
+++ server.js
@@ -19,6 +19,14 @@ app.get('/api/search_phrases', async (req, res) => {
   }
 });
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 app.listen(process.env.PORT || 5000, () =>
   console.log(`Listening to port ${process.env.PORT || 5000}`)
 );
